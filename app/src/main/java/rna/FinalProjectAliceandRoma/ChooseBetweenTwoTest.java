837c92358bcf5c9673a532aba6cebe566dcaa8b6
Loveseo8 <maggaket@gmail.com>
2020-05-10 14:12:55 +0300

    second

---
+++ app/src/main/java/rna/FinalProjectAliceandRoma/ChooseBetweenTwoTest.java
@@ -0,0 +1,14 @@
+package rna.FinalProjectAliceandRoma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseBetweenTwoTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_between_two_test);
+    }
+}
---
+++ app/src/main/java/rna/FinalProjectAliceandRoma/MatchImagesAndWordsTest.java
@@ -0,0 +1,14 @@
+package rna.FinalProjectAliceandRoma;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MatchImagesAndWordsTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_images_and_words_test);
+    }
+}
