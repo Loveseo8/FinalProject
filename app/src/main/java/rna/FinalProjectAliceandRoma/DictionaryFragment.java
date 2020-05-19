40d8a569e5551b92c65f6b753f9f89d60fc6b04f
Loveseo8 <maggaket@gmail.com>
2020-05-19 22:30:06 +0300

    second

--- app/src/main/java/rna/FinalProjectAliceandRoma/DictionaryFragment.java
+++ app/src/main/java/rna/FinalProjectAliceandRoma/DictionaryFragment.java
@@ -4,22 +4,28 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
-import java.util.ArrayList;
-import java.util.List;
 
-public class DictionaryFragment extends Fragment implements RecyclerViewAdapter.ItemClickListener {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-    RecyclerViewAdapter adapter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DictionaryFragment extends Fragment {
+
+    DatabaseReference wordsDatabase;
+
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
     Button add_word;
-    EditText enter_word;
+    EditText editTextWord;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,32 +37,60 @@ public class DictionaryFragment extends Fragment implements RecyclerViewAdapter.
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        final List<String> settings = new ArrayList<>();
+        wordsDatabase = FirebaseDatabase.getInstance().getReference();
 
         add_word = (Button) view.findViewById(R.id.button_add_word);
+        editTextWord = (EditText) view.findViewById(R.id.editTextWord);
         add_word.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
 
-                settings.add("apple");
+                String input = editTextWord.getText().toString();
+                String [] word = input.split(" ");
+                writeNewWord(word[0], word[1]);
 
             }
         });
 
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        adapter = new RecyclerViewAdapter(settings, getContext());
-        adapter.setClickListener(this);
-        recyclerView.setAdapter(adapter);
-
     }
 
-    @Override
-    public void onItemClick(View view, int position) {
+    private void writeNewWord(String word, String translation) {
 
-        Toast.makeText(getContext(), "You clicked " + adapter.getItem(position) + " on row number " + position, Toast.LENGTH_LONG).show();
+
+        String key = wordsDatabase.child("words").push().getKey();
+        Word word1 = new Word(word, translation);
+        Map<String, Object> wordValues = word1.toMap();
+
+        Map<String, Object> childUpdates = new HashMap<>();
+        childUpdates.put("/user-word/" + user.getUid() + "/" + key, wordValues);
+
+        wordsDatabase.updateChildren(childUpdates);
+
+    }
+}
+
+class Word {
+
+    String word;
+    String translation;
+
+    public Word() {
+    }
+
+    public Word(String word, String translation) {
+
+        this.word = word;
+        this.translation = translation;
+    }
+
+    public Map<String, Object> toMap() {
+
+        HashMap<String, Object> word = new HashMap<>();
+
+        word.put("word", word);
+        word.put("translation", translation);
+
+        return word;
 
     }
 
