d02ca60cfcf2f8be0d065f572bc48bd798ec4f22
Loveseo8 <maggaket@gmail.com>
2020-05-21 15:23:20 +0300

    second

--- app/src/main/java/rna/FinalProjectAliceandRoma/DictionaryFragment.java
+++ app/src/main/java/rna/FinalProjectAliceandRoma/DictionaryFragment.java
@@ -18,8 +18,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -57,7 +60,44 @@ public class DictionaryFragment extends Fragment implements RecyclerViewAdapter.
             }
         });
 
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        databaseWords.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                showData(dataSnapshot);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void showData(DataSnapshot dataSnapshot) {
+
+        myWords.clear();
+
+
+        for (DataSnapshot ds : dataSnapshot.child(user.getUid()).getChildren()) {
+
+            for (DataSnapshot dataSnapshot1 : ds.getChildren()) {
+
+                myWords.add(dataSnapshot1.getValue().toString());
+
+            }
+
+        }
+
+        RecyclerView recyclerView = (RecyclerView) getView().findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         adapter = new RecyclerViewAdapter(myWords, getContext());
@@ -68,23 +108,18 @@ public class DictionaryFragment extends Fragment implements RecyclerViewAdapter.
 
     private void addWord() {
 
-        String word = editTextWord.getText().toString().trim();
+        String text_word = editTextWord.getText().toString().trim();
 
-        if (TextUtils.isEmpty(word)) {
+        if (TextUtils.isEmpty(text_word)) {
 
-            Snackbar snackbar = Snackbar.make(getActivity().findViewById(R.id.dictionary_layout), "Enter word!", Snackbar.LENGTH_LONG);
-            snackbar.show();
+            editTextWord.setError("Enter word!");
+            editTextWord.requestFocus();
 
         } else {
 
             String id = databaseWords.push().getKey();
-            Word word1 = new Word(word, id, user.getUid());
-            databaseWords.child(user.getUid()).child(id).setValue(word1);
-
-            myWords.add(word1.getWord());
-
-            Snackbar snackbar = Snackbar.make(getActivity().findViewById(R.id.dictionary_layout), "Word added!", Snackbar.LENGTH_LONG);
-            snackbar.show();
+            Word word = new Word(text_word);
+            databaseWords.child(user.getUid()).child(id).setValue(word);
 
         }
 
@@ -102,17 +137,13 @@ public class DictionaryFragment extends Fragment implements RecyclerViewAdapter.
 class Word {
 
     String word;
-    String wordID;
-    String userID;
 
     public Word() {
     }
 
-    public Word(String word, String wordID, String userID) {
+    public Word(String word) {
 
         this.word = word;
-        this.wordID = wordID;
-        this.userID = userID;
 
     }
 
@@ -122,15 +153,9 @@ class Word {
 
     }
 
-    public String getWordID() {
+    public void setWord(String word) {
 
-        return wordID;
-
-    }
-
-    public String getUserID() {
-
-        return userID;
+        this.word = word;
 
     }
 }
